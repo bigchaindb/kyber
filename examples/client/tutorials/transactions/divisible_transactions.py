f678171922fb19395e0d7b4a219d090295fc360e
diminator <dimi.dejonghe@gmail.com>
2017-02-17 22:33:50 +0100

    documentation divisible_transactions

--- examples/client/tutorials/transactions/divisible_transactions.py
+++ examples/client/tutorials/transactions/divisible_transactions.py
@@ -21,6 +21,7 @@ alice = generate_keypair()
 bob = generate_keypair()
 carly = generate_keypair()
 
+# assign a simple coin with amount 4
 tx_create_alice_divisible = bdb.transactions.prepare(
     operation='CREATE',
     signers=alice.public_key,
@@ -30,12 +31,16 @@ tx_create_alice_divisible = bdb.transactions.prepare(
 tx_create_alice_divisible_signed = bdb.transactions.fulfill(
     tx_create_alice_divisible, private_keys=alice.private_key)
 
-asset_id = tx_create_alice_divisible_signed['id']
-
 print('Posting signed transaction{}'.format(tx_create_alice_divisible_signed))
 res = bdb.transactions.send(tx_create_alice_divisible_signed)
 poll_status_and_fetch_transaction(tx_create_alice_divisible_signed['id'], driver=bdb)
 
+asset_id = tx_create_alice_divisible_signed['id']
+
+# divide the coin of 4 into 3 outputs:
+#     - 2 for carly
+#     - 1 for bob
+#     - 1 for alice (change)
 
 tx_transfer_divisible = prepare_transfer(
         inputs=[
@@ -78,6 +83,12 @@ print('Retrieve list of transactions with asset_id {}: {}'.format(asset_id, len(
 # res = bdb.outputs.get(public_key=carly.public_key, unspent=True)
 # print('Retrieve list of unspent outputs with public_key {}: {}'.format(carly.public_key, len(res)))
 
+# combine some coins:
+#     - 1 coin of amount 2 (carly)
+#     - 1 coin of amount 1 (bob)
+# and divide them:
+#     - 1 coin of amount 1 (carly)
+#     - 1 coin of amount 2 (alice)
 tx_transfer_divisible = prepare_transfer(
         inputs=[
             {
@@ -96,13 +107,14 @@ tx_transfer_divisible = prepare_transfer(
                 'amount': 1
             },
             {
-                'condition': Ed25519Fulfillment(public_key=Ed25519VerifyingKey(carly.public_key)),
-                'public_keys': [bob.public_key],
+                'condition': Ed25519Fulfillment(public_key=Ed25519VerifyingKey(alice.public_key)),
+                'public_keys': [alice.public_key],
                 'amount': 2
             }
         ]
 )
 
+# alice and bob both have to sign an input
 tx_transfer_divisible_signed = sign_ed25519(tx_transfer_divisible, [bob.private_key, carly.private_key])
 print('Posting signed transaction{}'.format(tx_transfer_divisible_signed))
 bdb.transactions.send(tx_transfer_divisible_signed)
