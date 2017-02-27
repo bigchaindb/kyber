7e5b2403f1033c4b912ca6ec1a83e07437026bfb
baibaratsky <andrei@baibaratsky.com>
2017-02-27 16:24:19 +0100

    +

--- app/migrations/2_deploy_contracts.js
+++ app/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var TokenMock = artifacts.require("./TokenMock.sol");
 var MobileEnergy = artifacts.require("./MobileEnergy.sol");
 
 module.exports = function(deployer) {
-  return deployer.deploy(TokenMock).then(
-    return deployer.deploy(MobileEnergy, TokenMock.address);
-  );
+  deployer.deploy(TokenMock).then(function() {
+    deployer.deploy(MobileEnergy, TokenMock.address);
+  });
 };
--- app/test/MobileEnergy.js
+++ app/test/MobileEnergy.js
@@ -2,11 +2,13 @@ var MobileEnergy = artifacts.require('./MobileEnergy.sol');
 
 contract('MobileEnergy', function(accounts) {
   it('should publish an offer',function() {
-    return MobileEnergy.deployed().then(function(instance) {
+    return MobileEnergy.deployed()
+    .then(function(instance) {
       return instance.publishOffer.call({from:accounts[0],_price: 100, _power: 15});
-    }).then(function() {
+    })
+    .then(function() {
       //assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
-      
+
     });
   });
   it('should accept an offer');
