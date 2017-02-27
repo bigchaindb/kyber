7993364713e0e3863557fa0c02e1120dcfa1d505
baibaratsky <andrei@baibaratsky.com>
2017-02-27 15:40:36 +0100

    Tests init

---
+++ app/test/MobileEnergy.js
@@ -0,0 +1,8 @@
+var MobileEnergy = artifacts.require('./MobileEnergy.sol');
+
+contract('MobileEnergy', function(accounts) {
+  it('should publish an offer');
+  it('should accept an offer');
+  it('should close a contract');
+  it('should withdraw funds from a contract');
+});
