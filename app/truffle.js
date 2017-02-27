05814f370ac40a8dcc0c59339ba27fe1751953f9
baibaratsky <andrei@baibaratsky.com>
2017-02-27 11:31:32 +0100

    Network settings

--- app/truffle.js
+++ app/truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    bs: {
+      host: "hackathon-team6.ukwest.cloudapp.azure.com",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   }
 };
